98d6d2771c1e23e6f4bf6e320cb123f6c0541abf
MartmatiX <martin.malir@gmail.com>
2021-03-03 14:47:09 +0100

    .

--- tasks/main_objective/not_finished-main_objective.php
+++ tasks/main_objective/not_finished-main_objective.php
@@ -2,7 +2,7 @@
 <?php
   if (isset($_POST['delete'])) {
     if ($db->run("DELETE FROM main_objectives WHERE id = ?", [$_POST['not_finished_id']])) {
-      header("Location: finished-main_objective.php");
+      header("Location: not_finished-main_objective.php");
       exit();
     }
   }
@@ -38,8 +38,8 @@
             </div>
             <div class="new_date">
               <form class="" method="post">
-                <input type="date" name="new_date" onfocus="(this.type='date')" onfocusout="(this.type='text')" placeholder="Datum">
-                <input type="submit" name="set_new_date" value="Nový datum">
+                <input style="width: 100%; margin-top: 20px;" type="text" class="finish_date" name="new_date" onfocus="(this.type='date')" onfocusout="(this.type='text')" placeholder="Datum">
+                <input style="margin-top: 30px; margin-left: 35%;" type="submit" class="form_send" name="set_new_date" value="Nový datum">
                 <input type="text" name="not_finished_id" value="<?php echo $not_finished_objective->id ?>" style="display:none;">
               </form>
             </div>
@@ -55,5 +55,24 @@
       </div>
     <?php endif; ?>
   <?php endif; ?>
+  <script type="text/javascript">
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth()+1; //January is 0!
+  let yyyy = today.getFullYear();
+  if(dd < 10){
+    dd = '0' + dd;
+  }
+  if(mm < 10){
+    mm = '0' + mm;
+  }
+  today = yyyy+'-'+mm+'-'+dd;
+
+  let objectives_array = document.getElementsByClassName("finish_date");
+  console.log(objectives_array);
+  for (var i = 0; i < objectives_array.length; i++) {
+    objectives_array[i].setAttribute("min", today);
+  }
+  </script>
 </main>
 <?php require_once '../../footer.php'; ?>
